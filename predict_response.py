c275309a764d9c901995d42e77130b18ecbe3b41
DanyOso53 <98567163+DanyOso53@users.noreply.github.com>
2023-07-04 22:54:42 -0600

    Add files via upload

---
+++ predict_response.py
@@ -0,0 +1,61 @@
+import nltk
+
+import json
+import pickle
+import numpy as np
+import random 
+import tensorflow
+
+
+ignore_words = ['?', '!',',','.', "'s", "'m"]
+model = tensorflow.keras.models.load_model("./chatbot_model.h5")
+intents = json.loads(open("./intents.json").read())
+words = pickle.load(open("./words.pkl","rb"))
+clases = pickle.load(open("./classes.pkl","rb"))
+
+from data_preprocessing import get_stem_words
+
+def preprocess_user_input(user_input):
+
+    input_word_token_1 = nltk.word_tokenize(user_input)
+    input_word_token_2 = get_stem_words(input_word_token_1, ignore_words) 
+    input_word_token_2 = sorted(list(set(input_word_token_2)))
+
+    bag=[]
+    bag_of_words = []
+   
+    # Codificación de los datos de entrada
+    for word in words:            
+        if word in input_word_token_2:              
+            bag_of_words.append(1)
+        else:
+            bag_of_words.append(0) 
+    bag.append(bag_of_words)
+  
+    return np.array(bag)
+
+def bot_class_prediction(user_input):
+
+    inp = preprocess_user_input(user_input)
+    prediction = model.predict(inp)
+    predicted_class_label = np.argmax(prediction[0])
+    return predicted_class_label
+
+
+def bot_response(user_input):
+
+   predicted_class_label =  bot_class_prediction(user_input)
+   predicted_class = clases[predicted_class_label]
+
+   for intent in intents['intents']:
+    if intent['tag']==predicted_class:
+        bot_response = random.choice(intent['responses'])
+        return bot_response
+
+
+print("Bienvenido")
+while True :
+    user_input = input("En que puedo ayudarte ?")
+    print("Usuario: ", user_input)
+    response = bot_response(user_input)
+    print("Daniel_robot: ", response)
